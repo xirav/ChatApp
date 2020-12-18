300cad35a1b1035495f18fb2246c341dfdecc450
Kuldeep R Patil <61541745+xirav@users.noreply.github.com>
2020-12-18 13:25:20 +0530

    Update django.sh

--- scripts/django.sh
+++ scripts/django.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo cp /usr/bin/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
+sudo systemctl start django
+sudo systemctl enable django
